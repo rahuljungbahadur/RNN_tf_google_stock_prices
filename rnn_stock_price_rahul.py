ee65303f5260a0d2a6ef4289d09a7e169e5153cb
Rahul Bahadur <rahulb@umd.edu>
2018-01-28 17:26:26 -0500

    Model trained
    Uses 4 LSTM layers of 50 units each and an output (Dense) layer of 1 unit.

--- rnn_stock_price_rahul.py
+++ rnn_stock_price_rahul.py
@@ -25,8 +25,61 @@ scale = MinMaxScaler(feature_range = (0,1))
 
 training_scaled = scale.fit_transform(training_set)
 
+##Creating a data structure with 60 timesteps and 1 output
+X_train = []
+y_train = []
+
+for i in range(60,1258):
+    X_train.append(training_scaled[i-60:i, 0])
+    y_train.append(training_scaled[i,0])
+
+X_train, y_train = np.array(X_train), np.array(y_train)
+
+
+##Reshaping
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
 ##Building the RNN
 
+##Importing Keras libraries
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+###
+
+regressor = Sequential()
+
+##Adding the first LSTM layer and Dropout layer
+
+regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+regressor.add(Dropout(0.2))
+
+##Adding the second LSTM layer
+
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+##Adding the third LSTM layer
+
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+##Adding the fourth LSTM layer
+
+regressor.add(LSTM(units = 50))
+regressor.add(Dropout(0.2))
+
+##Adding the output layer
+regressor.add(Dense(units = 1))
+
+##Compiling the RNN
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+##Fitting the RNN
+
+regressor.fit(X_train, y_train, batch_size = 32, epochs = 100)
 
 ##Making predictions and visualizing the results
 
